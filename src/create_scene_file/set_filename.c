29f89c287c3683c6701cfa714b67acacbc9bfe60
RJW <r.j.w.de.bruijn@gmail.com>
2026-06-18 19:23:07 +0200

    perf: optimize scaled pixel write with packed 32-bit RGBA

--- src/create_scene_file/set_filename.c
+++ src/create_scene_file/set_filename.c
@@ -23,7 +23,6 @@ void	set_filename(const keys_t key, t_window *win, t_scene *sc)
 			create_scene_rt_file(sc, win->filename + (sizeof(NAME_FILE) - 1));
 			cpy_str0(win->filename + 6, "Created file: ");
 			win->filename[sizeof(NAME_FILE) - 2] = ' ';
-			// mlx_set_window_title(win->mlx, win->filename + 6);
 		}
 		mlx_set_window_title(win->mlx, "miniRT");
 		reset_filename(win);
--- src/render/rendering/set_pixel.c
+++ src/render/rendering/set_pixel.c
@@ -1,13 +1,18 @@
 #include "RTmlx.h"
 #include "render.h"
 
+//	Static Function
+static inline uint32_t	pack_rgba(t_vec4 color);
+
 void	scaled_res_set_pixel(t_window *w, uint16_t x, uint16_t y, t_vec4 color)
 {
-	uint8_t				*pixels;
-	t_axis2				i;
-	t_axis2				pixel;
+	uint8_t		*pixels;
+	t_axis2		i;
+	t_axis2		pixel;
+	uint32_t	packed;
 
 	color *= 255.0F;
+	packed = pack_rgba(color);
 	i.y = 0;
 	while (i.y < w->res_ratio)
 	{
@@ -19,10 +24,7 @@ void	scaled_res_set_pixel(t_window *w, uint16_t x, uint16_t y, t_vec4 color)
 			if (pixel.x < w->mlx->width && pixel.y < w->mlx->height)
 			{
 				pixels = w->pixels + (pixel.y * w->mlx->width + pixel.x) * 4;
-				*(pixels++) = (uint8_t)color[R];
-				*(pixels++) = (uint8_t)color[G];
-				*(pixels++) = (uint8_t)color[B];
-				*(pixels++) = (uint8_t)color[A];
+				*(uint32_t *)pixels = packed;
 			}
 			++i.x;
 		}
@@ -32,29 +34,38 @@ void	scaled_res_set_pixel(t_window *w, uint16_t x, uint16_t y, t_vec4 color)
 
 void	set_pixel_multi(t_thread *th, uint16_t res_ratio, t_axis2 inp, t_vec4 color)
 {
-	uint8_t	*pixels;
-	t_axis2	i;
-	t_axis2	pix;
+	uint8_t		*pixels;
+	t_axis2		i;
+	t_axis2		pixel;
+	uint32_t	packed;
 
 	color *= 255.0F;
+	packed = pack_rgba(color);
 	i.y = 0;
 	while (i.y < res_ratio)
 	{
 		i.x = 0;
-		pix.y = (uint16_t)(inp.y * res_ratio + i.y);
+		pixel.y = (uint16_t)(inp.y * res_ratio + i.y);
 		while (i.x < res_ratio)
 		{
-			pix.x = (uint16_t)(inp.x * res_ratio + i.x);
-			if (pix.x < th->img->width && pix.y < th->img->height)
+			pixel.x = (uint16_t)(inp.x * res_ratio + i.x);
+			if (pixel.x < th->img->width && pixel.y < th->img->height)
 			{
-				pixels = th->pixels + (pix.y * th->img->width + pix.x) * 4;
-				*(pixels++) = (uint8_t)color[R];
-				*(pixels++) = (uint8_t)color[G];
-				*(pixels++) = (uint8_t)color[B];
-				*(pixels++) = (uint8_t)color[A];
+				pixels = th->pixels + (pixel.y * th->img->width + pixel.x) * 4;
+				*(uint32_t *)pixels = packed;
 			}
 			++i.x;
 		}
 		++i.y;
 	}
+}
+
+static inline uint32_t	pack_rgba(t_vec4 color)
+{
+	return (
+		((uint32_t)(uint8_t)color[R]) |
+		((uint32_t)(uint8_t)color[G] << 8) |
+		((uint32_t)(uint8_t)color[B] << 16) |
+		((uint32_t)(uint8_t)color[A] << 24)
+	);
 }
